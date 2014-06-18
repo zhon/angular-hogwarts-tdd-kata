6d8d382ce49e909988bfa5953ac760dc1c18b730
zhon <zhon@xputah.org>
2014-06-18 11:12:35 -0600

    1.1.erroring

--- test/catalog/catalog-controller-specs.js
+++ test/catalog/catalog-controller-specs.js
@@ -2,6 +2,13 @@
 
 describe('CatalogController', function () {
 
+    describe('when the controller first loads', function () {
+
+        it('the course catalog is retrieved', function () {
+            sinon.assert.calledOnce(mockCatalogRepository.getCatalog);
+        });
+
+    });
 
 });
 
