a80a09d58b5760315e1d652a3edcc2624ee58ccb
zhon <zhon@xputah.org>
2014-06-18 15:10:47 -0600

    2.1.passing

--- app/catalog/catalog-controller.js
+++ app/catalog/catalog-controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 hogwartsApp
-.controller("CatalogController", function ($scope, catalogRepository) {
+.controller("CatalogController", function ($scope, catalogRepository, registrationService) {
     $scope.catalog = catalogRepository.getCatalog();
+
+    $scope.register = function(courseId) {
+        registrationService.register(courseId);
+    };
 });
