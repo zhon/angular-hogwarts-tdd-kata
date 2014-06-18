9a159e9f74b8a3ec701330512e72d4daedf4550d
zhon <zhon@xputah.org>
2014-06-18 15:19:01 -0600

    2.3.passing

--- app/wizard/registration-service.js
+++ app/wizard/registration-service.js
@@ -1,9 +1,15 @@
 'use strict';
 
 hogwartsApp
-.factory('registrationService', function() {
+.factory('registrationService', function(catalogRepository, wizardRepository) {
     return {
         register: function(courseId) {
+            var course = catalogRepository.getCourse(courseId),
+                wizard = wizardRepository.get();
+
+            wizard.courses[course.id] = course;
+            wizardRepository.save(wizard);
         }
     };
+
 });
