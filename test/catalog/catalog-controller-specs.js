b9d43c90e80a49a69b91012f86548f3a94b958a9
zhon <zhon@xputah.org>
2014-06-18 11:15:05 -0600

    1.1.failing

--- test/catalog/catalog-controller-specs.js
+++ test/catalog/catalog-controller-specs.js
@@ -2,6 +2,26 @@
 
 describe('CatalogController', function () {
 
+    var mockCatalogRepository,
+        scope,
+        catalog = ["catalog"];
+
+    beforeEach(function () {
+        module("hogwartsApp");
+
+        inject(function ($rootScope, $controller, catalogRepository) {
+            scope = $rootScope.$new();
+
+            mockCatalogRepository = sinon.stub(catalogRepository);
+            mockCatalogRepository.getCatalog.returns(catalog);
+
+            $controller("CatalogController", {
+                $scope: scope,
+                catalogRepository: mockCatalogRepository
+            });
+        });
+    });
+
     describe('when the controller first loads', function () {
 
         it('the course catalog is retrieved', function () {
