9b501501480f966c9d9a4e46a7a0618bb272fc29
zhon <zhon@xputah.org>
2014-06-18 15:18:10 -0600

    2.3.failing

--- test/wizard/registration-service-specs.js
+++ test/wizard/registration-service-specs.js
@@ -2,8 +2,27 @@
 
 describe('registrationService', function () {
 
+    var service,
+        mockCatalogRepository,
+        mockWizardRepository;
+
+    beforeEach(function () {
+        module("hogwartsApp");
+
+        inject(function (registrationService, catalogRepository, wizardRepository) {
+            service = registrationService;
+            mockCatalogRepository = sinon.stub(catalogRepository);
+            mockWizardRepository = sinon.stub(wizardRepository);
+        });
+    });
+
     describe('when registering for a course', function () {
         var course = {id: 'Potions'};
+
+        beforeEach(function() {
+            mockCatalogRepository.getCourse.returns(course);
+            mockWizardRepository.get.returns({courses: {}});
+        });
 
         it ('saves the course to the wizardRepository', function() {
             service.register(course.id);
