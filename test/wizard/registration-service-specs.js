31c547d579370dc5763d97a9f832ec3cf4704e59
zhon <zhon@xputah.org>
2014-06-18 15:16:27 -0600

    2.3.erroring

--- test/wizard/registration-service-specs.js
+++ test/wizard/registration-service-specs.js
@@ -2,4 +2,16 @@
 
 describe('registrationService', function () {
 
+    describe('when registering for a course', function () {
+        var course = {id: 'Potions'};
+
+        it ('saves the course to the wizardRepository', function() {
+            service.register(course.id);
+            sinon.assert.calledWith(
+                mockWizardRepository.save, {courses: {'Potions' : course}}
+            );
+        });
+
+    });
+
 });
